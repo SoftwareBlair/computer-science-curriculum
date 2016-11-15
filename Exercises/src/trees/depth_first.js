97d1559bbffb9a26a599d3883f0dc636574f62dd
Brandon Blair <brandon.d.blair@gmail.com>
2016-11-15 16:08:00 -0700

    Completed depth_first exercise

--- Exercises/src/trees/depth_first.js
+++ Exercises/src/trees/depth_first.js
@@ -10,7 +10,20 @@
   Write a recursive algorithm that returns its values as an array, ordered using a depth first search, pre-order strategy.
 */
 module.exports.preOrder = function(root) {
+  let data = [];
+  let current = root;
 
+  function search(node) {
+    data.push(node.data);
+    if (node.left) {
+      search(node.left);
+    }
+    if (node.right) {
+      search(node.right);
+    }
+  }
+  search(current);
+  return data;
 };
 
 /*
@@ -25,7 +38,21 @@ module.exports.preOrder = function(root) {
   Write a recursive algorithm that returns its values as an array, ordered using a depth first search, in-order strategy.
 */
 module.exports.inOrder = function(root) {
+  let data = [];
+  let current = root;
 
+  function search(node) {
+    if (node.left) {
+      search(node.left);
+    }
+    data.push(node.data);
+    if (node.right) {
+      search(node.right);
+    }
+  }
+
+  search(current);
+  return data;
 };
 
 /*
@@ -40,5 +67,19 @@ module.exports.inOrder = function(root) {
   Write a recursive algorithm that returns its values as an array, ordered using a depth first search, post-order strategy.
 */
 module.exports.postOrder = function(root) {
+  let data = [];
+  let current = root;
 
+  function search(node) {
+    if (node.left) {
+      search(node.left);
+    }
+    if (node.right) {
+      search(node.right);
+    }
+    data.push(node.data);
+  }
+
+  search(current);
+  return data;
 };
